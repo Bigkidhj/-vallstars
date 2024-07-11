67a3b422231dd9a7f03b298a5a8c1b1173a53840
JangHyukjun <j2261305@gmail.com>
2024-07-11 10:54:14 +0900

    팀원이 팀을 선택하면 해당 팀으로 이동

--- TeamChoice/Assets/Scripts/ChoiceManager.cs
+++ TeamChoice/Assets/Scripts/ChoiceManager.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class ChoiceManager : MonoBehaviour
 {
     public TeamManager teamManager;
-    public TeamManager.TeamMember teamMember;
+    public TeamManager.TeamMember currentTeamMember;
+    public int currentMemberIndex = 0;
     public void Start()
     {
         //TeamManager를 찾아서 teamManager에 할당
         teamManager = GameObject.Find("TeamManager").GetComponent<TeamManager>();
+    }
+
+    public void LoadNextMember()
+    {
+        //팀원 리스트가 남아있다면 다음 멤버 호출
+        if(currentMemberIndex < 19)
+        {
+            currentMemberIndex++;
+            teamManager.LoadNextMember(currentMemberIndex);
+        }
+        else
+        {
+            Debug.Log("더 이상 팀원이 없습니다.");
+        }
+    }
+
+    // 현재 teamMember를 teamManager의 특정 Team 안에 있는 teamMembers 리스트에 추가
+    public void AddMemberToTeam(int TeamIndex)
+    {
+        teamManager.AddMemberToTeam(TeamIndex);
     }
 }
--- TeamChoice/Assets/Scripts/TeamManager.cs
+++ TeamChoice/Assets/Scripts/TeamManager.cs
@@ -8,7 +8,7 @@ using TMPro;
 
 public class TeamManager : MonoBehaviour
 {
-    // 팀장 클래스 정의
+    // 팀장 정보를 담는 클래스
     [System.Serializable]
     public class TeamLeader
     {
@@ -21,57 +21,55 @@ public class TeamManager : MonoBehaviour
             this.order = order;
         }
     }
-
-    // 팀원 클래스 정의
+    // 팀원 정보를 담는 클래스
     [System.Serializable]
     public class TeamMember
     {
         public string name;
         public int order;
+        public string imageName;
 
-        public TeamMember(string name, int order)
+        public TeamMember(string name, int order, string imageName)
         {
             this.name = name;
             this.order = order;
+            this.imageName = imageName;
+        }
+    }
+    // 팀 정보를 담는 클래스
+    [System.Serializable]
+    public class Team
+    {
+        public List<TeamLeader> leader;
+        public List<TeamMember> members;
+
+        public Team(List<TeamLeader> leader, List<TeamMember> members)
+        {
+            this.leader = leader;
+            this.members = members;
         }
     }
 
-    //팀 클래스 정의
-    [System.Serializable]
-      public class Team
-        {
-            public List<TeamLeader> leader;
-            public List<TeamMember> members;
-
-            public Team(List<TeamLeader> leader, List<TeamMember> members)
-            {
-                this.leader = leader;
-                this.members = members;
-            }
-        }
-
-    // 팀장과 팀원 리스트
     public List<TeamLeader> teamLeaders;
     public List<TeamMember> teamMembers;
-    // 팀장 이미지 리스트
     public List<Image> leaderImages;
-    // 팀장 이름 리스트
     public List<TMP_Text> leaderNames;
-    // 팀 리스트
-    public Team teamA;
-    public Team teamB;
-    public Team teamC;
-    public Team teamD;
-    public Team teamE;    
-    public Team teamF;
-    public Team teamG;
-    public Team teamH;
-    public Team teamI;
-    public Team teamJ;
+    public List<Team> teams;
+    public List<List<Image>> teamMemberImages;
+    public List<List<TMP_Text>> teamMemberNames;
+
+    private const int MaxMembersPerTeam = 2;
 
     public void Start()
     {
-        // 팀장 리스트 초기화
+        // 팀장, 팀원, 팀 정보 초기화
+        InitializeTeams();
+        InitializeUI();
+        LoadFirstMember();
+    }
+
+    private void InitializeTeams()
+    {
         teamLeaders = new List<TeamLeader>
         {
             new TeamLeader("팀장A", 0),
@@ -86,63 +84,113 @@ public class TeamManager : MonoBehaviour
             new TeamLeader("팀장J", 9)
         };
 
-        // 팀원 리스트 초기화
         teamMembers = new List<TeamMember>
         {
-            new TeamMember("팀원1", 0),
-            new TeamMember("팀원2", 1),
-            new TeamMember("팀원3", 2),
-            new TeamMember("팀원4", 3),
-            new TeamMember("팀원5", 4),
-            new TeamMember("팀원6", 5),
-            new TeamMember("팀원7", 6),
-            new TeamMember("팀원8", 7),
-            new TeamMember("팀원9", 8),
-            new TeamMember("팀원10", 9),
-            new TeamMember("팀원11", 10),
-            new TeamMember("팀원12", 11),
-            new TeamMember("팀원13", 12),
-            new TeamMember("팀원14", 13),
-            new TeamMember("팀원15", 14),
-            new TeamMember("팀원16", 15),
-            new TeamMember("팀원17", 16),
-            new TeamMember("팀원18", 17),
-            new TeamMember("팀원19", 18),
-            new TeamMember("팀원20", 19)
+        new TeamMember("팀원1", 0, "팀원1"),
+        new TeamMember("팀원2", 1, "팀원2"),
+        new TeamMember("팀원3", 2, "팀원3"),
+        new TeamMember("팀원4", 3, "팀원4"),
+        new TeamMember("팀원5", 4, "팀원5"),
+        new TeamMember("팀원6", 5, "팀원6"),
+        new TeamMember("팀원7", 6, "팀원7"),
+        new TeamMember("팀원8", 7, "팀원8"),
+        new TeamMember("팀원9", 8, "팀원9"),
+        new TeamMember("팀원10", 9, "팀원10"),
+        new TeamMember("팀원11", 10, "팀원11"),
+        new TeamMember("팀원12", 11, "팀원12"),
+        new TeamMember("팀원13", 12, "팀원13"),
+        new TeamMember("팀원14", 13, "팀원14"),
+        new TeamMember("팀원15", 14, "팀원15"),
+        new TeamMember("팀원16", 15, "팀원16"),
+        new TeamMember("팀원17", 16, "팀원17"),
+        new TeamMember("팀원18", 17, "팀원18"),
+        new TeamMember("팀원19", 18, "팀원19"),
+        new TeamMember("팀원20", 19, "팀원20")
         };
 
-        //각 팀 리스트에 팀장과 팀원 리스트를 할당, 단 팀장은 한명씩 할당되며 팀원 리스트는 비어있음
-        teamA = new Team(teamLeaders.GetRange(0, 1), new List<TeamMember>());
-        teamB = new Team(teamLeaders.GetRange(1, 1), new List<TeamMember>());
-        teamC = new Team(teamLeaders.GetRange(2, 1), new List<TeamMember>());
-        teamD = new Team(teamLeaders.GetRange(3, 1), new List<TeamMember>());
-        teamE = new Team(teamLeaders.GetRange(4, 1), new List<TeamMember>());
-        teamF = new Team(teamLeaders.GetRange(5, 1), new List<TeamMember>());
-        teamG = new Team(teamLeaders.GetRange(6, 1), new List<TeamMember>());
-        teamH = new Team(teamLeaders.GetRange(7, 1), new List<TeamMember>());
-        teamI = new Team(teamLeaders.GetRange(8, 1), new List<TeamMember>());
-        teamJ = new Team(teamLeaders.GetRange(9, 1), new List<TeamMember>());
-        
+        teams = new List<Team>();
+        for (int i = 0; i < teamLeaders.Count; i++)
+        {
+            teams.Add(new Team(new List<TeamLeader> { teamLeaders[i] }, new List<TeamMember>()));
+        }
+    }
 
-        // 팀장 이름 리스트의 텍스트를 팀장 리스트에 있는 이름으로 설정
+    private void InitializeUI()
+    {
         for (int i = 0; i < leaderNames.Count && i < teamLeaders.Count; i++)
         {
             leaderNames[i].text = teamLeaders[i].name;
         }
-        //팀장 이미지 리스트의 이미지를 게임오브젝트와 이름이 같은 이미지를 에셋의 이미지 폴더 안에 있는 TeamLeader 폴더에서 찾아서 할당
+
         for (int i = 0; i < leaderImages.Count && i < teamLeaders.Count; i++)
         {
             leaderImages[i].sprite = Resources.Load<Sprite>("Images/TeamLeader/" + teamLeaders[i].name);
         }
-        // 첫번째 멤버 호출
-        LoadFirstMember();
+
+        teamMemberImages = new List<List<Image>>();
+        for (int i = 0; i < teams.Count; i++)
+        {
+            teamMemberImages.Add(new List<Image>());
+            for (int j = 0; j < MaxMembersPerTeam; j++)
+            {
+                teamMemberImages[i].Add(GameObject.Find("Team" + i + "Member" + j + "Image").GetComponent<Image>());
+            }
+        }
+
+        teamMemberNames = new List<List<TMP_Text>>();
+        for (int i = 0; i < teams.Count; i++)
+        {
+            teamMemberNames.Add(new List<TMP_Text>());
+            for (int j = 0; j < MaxMembersPerTeam; j++)
+            {
+                teamMemberNames[i].Add(GameObject.Find("Team" + i + "Member" + j + "Name").GetComponent<TMP_Text>());
+            }
+        }
     }
 
-    //teammembers에 있는 첫번째 팀원을 ChoiceManager의 teamMember에 할당
     public void LoadFirstMember()
     {
+        // 첫번째 멤버를 불러오고 초기화
         ChoiceManager choiceManager = GameObject.Find("ChoiceManager").GetComponent<ChoiceManager>();
-        choiceManager.teamMember = teamMembers[0];
+        choiceManager.currentTeamMember = teamMembers[0];
+        Debug.Log(choiceManager.currentTeamMember.name);
+        Debug.Log(choiceManager.currentTeamMember.order);
     }
 
+    public void LoadNextMember(int index)
+    {
+        // 다음 멤버를 불러오고 초기화
+        ChoiceManager choiceManager = GameObject.Find("ChoiceManager").GetComponent<ChoiceManager>();
+        choiceManager.currentTeamMember = teamMembers[index];
+        Debug.Log(choiceManager.currentTeamMember.name);
+        Debug.Log(choiceManager.currentTeamMember.order);
+    }
+
+    public void AddMemberToTeam(int teamIndex)
+    {
+        // 팀에 멤버 추가
+        ChoiceManager choiceManager = GameObject.Find("ChoiceManager").GetComponent<ChoiceManager>();
+        Team team = teams.FirstOrDefault(t => t.leader[0].order == teamIndex);
+
+        if (team != null && team.members.Count < MaxMembersPerTeam)
+        {
+            team.members.Add(choiceManager.currentTeamMember);
+            UpdateTeamUI(teamIndex, team.members.Count - 1, choiceManager.currentTeamMember);
+            choiceManager.LoadNextMember();
+        }
+    }
+
+    private void UpdateTeamUI(int teamIndex, int memberIndex, TeamMember member)
+    {
+        string imagePath = "Images/TeamMember/" + member.imageName;
+        Sprite memberSprite = Resources.Load<Sprite>(imagePath);
+        if (memberSprite == null)
+        {
+            Debug.LogError("Failed to load sprite at path: " + imagePath);
+            return;
+        }
+        Debug.Log(teamIndex + memberIndex);
+        teamMemberImages[teamIndex][memberIndex].sprite = memberSprite;
+        teamMemberNames[teamIndex][memberIndex].text = member.name;
+    }
 }
